046895fa45eca15e721c9df9cc4f0cb7ffe904e4
Umar-www <davronali84@gmail.com>
2023-10-09 17:32:36 +0500

    test

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,20 +9,18 @@ export default function Routes() {
 
   return (
     <Switch>
-      <Route path="" element={!userToken ? <Navigate to="auth"/> : <Navigate to="dashboard"/>}/>
-       
-  
+      <Route path="" element={!userToken ? <Navigate to="auth" /> : <Navigate to="dashboard" />} />
 
-      <Route path="auth" element={!userToken ? <Outlet/> : <Navigate to="/dashboard"/>}>
+      <Route path="auth" element={!userToken ? <Outlet /> : <Navigate to="/dashboard" />}>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route index  path="*" element={<Navigate to="/auth/login"/>} />
+        <Route index path="*" element={<Navigate to="/auth/login" />} />
       </Route>
 
-      <Route path="dashboard" element={userToken ? <Outlet/> : <Navigate to="/auth"/>}>
+      <Route path="dashboard" element={userToken ? <Outlet /> : <Navigate to="/auth" />}>
         <Route path="products" element={<Products />} />
         <Route path="save" element={<Save />} />
-        <Route path="*" index element={<Navigate to="/dashboard/products"/>} />
+        <Route path="*" index element={<Navigate to="/dashboard/products" />} />
       </Route>
     </Switch>
   )
